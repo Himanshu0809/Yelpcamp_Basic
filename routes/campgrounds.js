34db6cf0e3166ddf04da0a83b5da8642b2acd4ec
Himanshu Gupta <guptahimanshu479@gmail.com>
2020-04-14 15:37:24 +0530

    Yelpcamp

---
+++ routes/campgrounds.js
@@ -0,0 +1,94 @@
+ var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campgrounds");
+var middleware =require("../middleware");
+
+//INDEX - show all campgrounds
+router.get("/", function (req, res) {
+    //Get all campgrounds from the DB
+    Campground.find({}, function (err, allcampgrounds) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render("campgrounds/index", { campgrounds: allcampgrounds, currentUser: req.user });
+        }
+    })
+
+});
+
+//CREATE - add new campground to the DB
+router.post("/",middleware.isLoggedIn, function (req, res) {
+    var name = req.body.name;
+    var price =req.body.price;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = { name: name,price:price, image: image, description: desc, author: author };
+    //Create a new Campground and save to DB
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect it to the campgrounds page
+
+            res.redirect("/campgrounds");
+            //by default the redirect is GET
+        }
+    });
+
+});
+
+//NEW - show form to create new campground
+router.get("/new",middleware.isLoggedIn, function (req, res) {
+    res.render("campgrounds/new")
+});
+
+//SHOW - show more info about one campground
+router.get("/:id", function (req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    });
+});
+
+//EDIT CAMPGROUND ROUTE
+router.get("/:id/edit",middleware.checkCampgroundOwnership, function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        res.render("campgrounds/edit", { campground: foundCampground });
+    });
+});
+
+//UPDATE CAMPGROUND ROUTE
+router.put("/:id",middleware.checkCampgroundOwnership ,function (req, res) {
+    //find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id",middleware.checkCampgroundOwnership , function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    })
+}); 
+
+module.exports = router;
